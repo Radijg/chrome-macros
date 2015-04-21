fed4afc855c3671df1cc5004d90e12c3f796a2d2
neaumusic <jsw.james.wilson@gmail.com>
2015-04-21 01:34:03 -0700

    Works for schemes http, https, and file, but unfortunately not chrome, chrome-extension, or about

--- ChromeOSTabber/ChromeOSTabber.js
+++ ChromeOSTabber/ChromeOSTabber.js
@@ -1,28 +1,11 @@
 window.addEventListener("keydown", function (e) {
-    if (e.keyCode === 221 && e.ctrlKey && e.shiftKey) {
-        tab(true);
-    } else if (e.keyCode === 219 && e.ctrlKey && e.shiftKey) {
-        tab(false);
+    var keydownEvent = {};
+
+    for (var key in e) {
+        if (e.hasOwnProperty(key) && typeof e[key] === "number" || typeof e[key] === "string" || typeof e[key] === "boolean") {
+            keydownEvent[key] = e[key]
+        }
     }
-});
 
-function tab (isForwardTab) {
-    window.console.log("tabbing forward: " + isForwardTab);
-
-    callWithTabID(function (tabID) {
-        console.log("active tab ID: ", tabID);
-    });
-
-    function callWithTabID (callback) {
-        chrome.tabs.query({
-            currentWindow: true,
-            active : true
-        }, function (tabArray) {
-            callback(tabArray[0]);
-        });
-    }
-}
-
-window.addEventListener("tabForward", function () {
-    console.log("TAB FORWARD RECEIVED MFCKER");
+    chrome.runtime.sendMessage({"keydownEvent": keydownEvent})
 });
